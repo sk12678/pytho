538c2a46204d4405daadbefa074029c93fab89b7
sk12678 <68110199+sk12678@users.noreply.github.com>
2020-08-29 22:27:33 +0800

    Add files via upload

---
+++ 功課9-4.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 29 22:03:10 2020
+
+@author: andy6
+"""
+import math
+
+n = int(input("請輸入座號"))
+for y in range(1,n+1,3):
+    continue
+print(math.ceil(y/3))
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
