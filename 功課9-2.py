db30f236088cca912ee429661d537431a5308132
sk12678 <68110199+sk12678@users.noreply.github.com>
2020-08-29 21:41:16 +0800

    Add files via upload

---
+++ 功課9-2.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 29 16:43:59 2020
+
+@author: andy6
+"""
+
+
+
+
+e = True
+x = float(input("輸入現在體重")) 
+y = x +15
+while e is True:
+    if x >= y :
+        e = False
+    else:
+        x = x + 1.5
+        print(x)
+    
+    
+   
+        
+
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
