7ad80c82232b898b4d5e2ab093021a8676c037ee
sk12678 <68110199+sk12678@users.noreply.github.com>
2020-08-29 22:51:35 +0800

    Add files via upload

---
+++ 功課10-1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 29 22:29:23 2020
+
+@author: andy6
+"""
+
+
+a = {"Sunday":"星期日",
+     "Monday":"星期一",
+     "Tuesday":"星期二",
+     "Wednesday" :"星期三",
+     "Thursday":"星期四",
+     "Friday":"星期五",
+     "Saturday":"星期六"}
+b = input("輸入星期英文")
+if b in a:
+    print(a[b])
+else:
+    print("輸入錯誤")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
