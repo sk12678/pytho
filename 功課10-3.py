08ec655b36835b5cc6d2af6241c457e81bfe6d72
sk12678 <68110199+sk12678@users.noreply.github.com>
2020-09-01 20:32:15 +0800

    Add files via upload

---
+++ 功課10-3.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 19:26:25 2020
+
+@author: andy6
+"""
+
+e = True
+noodles = {}
+a = input("輸入麵")
+f = int(input("輸入錢"))
+b = input("輸入麵")
+g = int(input("輸入錢"))
+c = input("輸入麵")
+h = int(input("輸入錢"))
+d = input("輸入麵")
+i = int(input("輸入錢"))
+e = input("輸入麵")
+j = int(input("輸入錢"))
+noodles[a] = f
+noodles[b] = g
+noodles[c] = h
+noodles[d] = i
+noodles[e] = j
+k = max(noodles)
+l = min(noodles)
+print(k,noodles[k])
+print(l,noodles[l])
+
+
+
+
+
+
+
