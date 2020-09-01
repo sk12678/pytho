97ba4e62b78921412fccdbffbeab9c49aa89c450
sk12678 <68110199+sk12678@users.noreply.github.com>
2020-09-01 21:00:21 +0800

    Add files via upload

---
+++ 功課10-3.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 19:26:25 2020
+
+@author: andy6
+"""
+
+x = {}
+a = input("輸入麵")
+f = input("輸入錢")
+b = input("輸入麵")
+g = input("輸入錢")
+c = input("輸入麵")
+h = input("輸入錢")
+d = input("輸入麵")
+i = input("輸入錢")
+e = input("輸入麵")
+j = input("輸入錢")
+x[a] = f
+x[b] = g
+x[c] = h
+x[d] = i
+x[e] = j
+print(zip(max(x.values())))
+print(zip(min(x.values())))
+
+
+
+
+
+
