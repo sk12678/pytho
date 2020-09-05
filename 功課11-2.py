e06938f3c67a68bf60316a7c771f578f8a0c122d
sk12678 <68110199+sk12678@users.noreply.github.com>
2020-09-05 23:53:09 +0800

    Add files via upload

---
+++ 功課11-2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 23:45:14 2020
+
+@author: andy6
+"""
+
+def add(a,b):
+    e = a+b
+    return(e)
+def sub(x,y):
+    e = x-y
+    return(e)
+def mul(m,n):
+    e = m*n
+    return(e)
+def div(n1,n2):
+    e = n1/n2
+    return(e)
+a = int(input("輸入數字"))
+b = int(input("輸入數字"))
+c = add(a,b)
+print(c)
+d = sub(a,b)
+print(d)
+g = mul(a,b)
+print(g)
+k = div(a,b)
+print(k)
+    
+    
+    
+    
+    
+    
