b501169ac396051bd56641d4a9f55fedc5ae6fef
sk12678 <68110199+sk12678@users.noreply.github.com>
2020-09-05 23:18:58 +0800

    Add files via upload

---
+++ 功課11-1.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 23:00:41 2020
+
+@author: andy6
+"""
+
+def mymax(x1,x2):
+    if x1>x2:
+        print(x1)
+    else:
+        print(x2)
+a = input("請輸入數字")
+b = input("請輸入數字")
+c = mymax(a,b)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
